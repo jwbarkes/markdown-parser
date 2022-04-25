bf199779c441d272635530e95654334083016662
jwbarkes <jbarkes@ucsd.edu>
2022-04-24 22:44:10 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,18 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            if(openBracket == -1){
+                break;
+            } 
+            if((openBracket > 0) && (markdown.charAt(openBracket - 1) == '!')){
+                currentIndex = openBracket + 1;
+                continue;
+            } 
             int closeBracket = markdown.indexOf("]", openBracket);
+            if(markdown.charAt(closeBracket + 1) != '('){
+                currentIndex = closeBracket + 1;
+                continue;
+            } 
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
